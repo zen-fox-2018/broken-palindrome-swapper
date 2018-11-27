89604f66bc7e8cbdf160ef9c797c79f42056b265
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-27 10:51:11 +0700

    komit

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -7,18 +7,22 @@
  */
 function palindromeSwapper(str) {
   let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+  
+  if (isPalindrome(str)) return true;
+
+  for (var i = 0; i < str.length-2; i++) {
+    for (var j = 0; j < str.length-2; i++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1];
         newStr += str[j];
-        j++;
       } else {
-        newStr += str[j];
+        j++;
+        newStr += str[j+1];
       }
     }
-    if (isPalindrome(newStr)) return true;
   }
+  if (isPalindrome(newStr)) return true;
+
   return false;
 }
 
@@ -29,8 +33,9 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  
+  if (str.split('').reverse().join('') === str) return true;
+  return false;
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
