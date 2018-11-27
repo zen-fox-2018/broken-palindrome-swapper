966159ff7adee15fd7e656447e269ebeedfa6631
maitreya <bluespider529@gmail.com>
2018-11-27 16:24:30 +0700

    done palindrome swapper

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -5,12 +5,16 @@
  * @param {string} str - input kata yang ingin di-swap dan dicek palindrom
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
+
 function palindromeSwapper(str) {
-  let newStr = '';
+  // debugger;
+  if(isPalindrome(str)) return true
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    let newStr = '';
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        // console.log(i, "============", j+1)
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
@@ -29,8 +33,13 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  // console.log("ini di split", str.split(''))
+  // console.log("ini di reverse", str.reverse())
+  if (str.split('').reverse().join("") === str) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
